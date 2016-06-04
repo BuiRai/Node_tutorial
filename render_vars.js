b835791d9ce3a1a348ad6acf05d89c18d5729b65
buirai <buirai_naetle@hotmail.com>
2016-06-04 11:55:16 -0500

    Lección completa

--- render_vars.js
+++ render_vars.js
@@ -8,8 +8,19 @@ var http = require("http"),
 
 http.createServer(function(req, res){
     fs.readFile("./index.html", function(err, html){
+
+        var html_str = html.toString();
+        var variables = html_str.match(/[^\{\}]+(?=\})/g);
+        var nombre = "Lucario";
+
+        // variable ["nombre"]
+        for (var i = variables.length - 1;  i >= 0 ; i--) {
+            var value = eval(variables[i]);
+            html_str = html_str.replace("{"+variables[i]+"}", value);
+        };
+
         res.writeHead(200, {"Content-Type" : "text/html"});
-        res.write(html);
+        res.write(html_str);
         res.end();
     });
 }).listen(8080, function(){
