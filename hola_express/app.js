a9a23d30e3326d9cd86c5836edb27011796deb06
buirai <buirai_naetle@hotmail.com>
2016-06-04 17:05:58 -0500

    Terminado :)

--- hola_express/app.js
+++ hola_express/app.js
@@ -2,8 +2,12 @@ var express = require("express");
 
 var app = express();
 
+app.set("view engine", "jade");
+
 app.get("/", function(req, res){
-	res.send("Hola mundo");
+	res.render("index", {
+		"hola": "Hola BuiRai"
+	});
 });
 
 app.listen(8080, function(){
