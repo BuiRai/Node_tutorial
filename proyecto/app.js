bbe2ff2ef6c237d7cd78097c925ca7bfc3f41754
buirai <buirai_naetle@hotmail.com>
2016-06-05 18:19:47 -0500

    Terminado :)

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,6 +1,19 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema; // La clase Schema viene de mongoose
+
+mongoose.connect("mongodb://localhost/fotolucario");
+
+var userSchemaJSON = {
+	email: String,
+	password: String
+};
+
+var user_schema = new Schema(userSchemaJSON); // Se crea un objeto que mongoose entiende
+
+var User = mongoose.model("User", user_schema); // Se crea un modelo y a partir de ello el documento en mongoDB
 
 app.use("/static", express.static("public"));
 // app.use("/static", express.static("assets"));
@@ -16,13 +29,21 @@ app.get("/", function(req, res){
 });
 
 app.get("/login", function(req, res){
+	User.find(function(err, data){
+		console.log(data);
+	});
 	res.render("login");
 });
 
 app.post("/users", function(req, res){
-	console.log("Email: " + req.body.user_email);
-	console.log("Password: " + req.body.user_password);
-	res.send("Recibimos tus datos");
+	var user = new User({
+		email: req.body.user_email,
+		password: req.body.user_password,
+	});
+
+	user.save(function(){
+		res.send("Guardamos tus datos");
+	});
 });
 
 app.listen(8080, function(){
