b69916bd919a373f1a2846322b701bbcf7634c01
buirai <buirai_naetle@hotmail.com>
2016-06-05 18:05:19 -0500

    Terminado :)

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,9 +1,13 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
 
 app.use("/static", express.static("public"));
-app.use("/static", express.static("assets"));
+// app.use("/static", express.static("assets"));
+
+app.use(bodyParser.json());
+// "extended: true" me permite parsear arrays y strings
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "jade");
 
@@ -13,6 +17,12 @@ app.get("/", function(req, res){
 
 app.get("/login", function(req, res){
 	res.render("login");
+});
+
+app.post("/users", function(req, res){
+	console.log("Email: " + req.body.user_email);
+	console.log("Password: " + req.body.user_password);
+	res.send("Recibimos tus datos");
 });
 
 app.listen(8080, function(){
