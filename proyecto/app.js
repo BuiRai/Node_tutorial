fcaabfb1339225a52b1d5738b63f864710723518
buirai <buirai_naetle@hotmail.com>
2016-06-06 15:19:16 -0500

    Terminado :)

--- proyecto/app.js
+++ proyecto/app.js
@@ -30,7 +30,10 @@ app.post("/users", function(req, res){
 		password_confirmation: req.body.user_password_confirmation
 	});
 
-	user.save(function(){
+	user.save(function(err){
+		if (err) {
+			console.log(String(err));
+		}
 		res.send("Guardamos tus datos");
 	});
 });
--- proyecto/models/user.js
+++ proyecto/models/user.js
@@ -3,15 +3,47 @@ var Schema = mongoose.Schema;
 
 mongoose.connect("mongodb://localhost/fotolucario");
 
+var email_match = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// var match = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
+
 // Se crea un objeto que mongoose entiende
 var user_schema = new Schema({
 	name: String,
-	username: String,
-	password: String,
-	age: Number,
-	email: String,
-	birthday: Date
-});
+	last_name: String,
+	// Validaciones
+	username: {
+		type: String,
+		required: true,
+		maxlength: [40, "Username muy grande"]
+	},
+	// Validaciones
+	password: {
+		type: String,
+		minlength: [4, "El password muy corto"]
+	},
+	// Validaciones
+	age: {
+		type: Number,
+		min: [4, "La edad no puede ser menor que 4"],
+		max: [100, "la edad no puede ser mayor a 100"],
+		match: [email_match, "COloca un Email válido"]
+	},
+	// Validaciones
+	email: {
+		type: String,
+		required: true /*"El correo es obligatorio"*/,
+		match: email_match
+	},
+	birthday: Date,
+	// Validaciones
+	gender: {
+		type: String,
+		enum: {
+			values: ["M", "F"],
+			message: "Opción no valida"
+		}
+	}
+})
 
 user_schema.virtual("password_confirmation").get(function(){
 	return this.user_password_confirmation;
