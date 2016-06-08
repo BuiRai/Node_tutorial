c450db92d65d691df33fdf98b41b762fd2fc8df6
buirai <buirai_naetle@hotmail.com>
2016-06-08 17:47:37 -0500

    Terminado :)

--- proyecto/routes_app.js
+++ proyecto/routes_app.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+var Image = require("./models/image");
 var router = express.Router();
 
 router.get("/", function(req, res){
@@ -17,7 +17,9 @@ router.get("/imagenes/:id/edit", function(req, res){ //Route to edit a new image
 // COnjunto de rutas para "/imagenes/:id"
 router.route("/imagenes/:id")
 	.get(function(req, res){
-
+		Image.findById(req.params.id, function(err, image){
+			res.render("app/imagenes/show", {image: image});
+		});
 	})
 	.put(function(req, res){
 
@@ -32,7 +34,17 @@ router.route("/imagenes")
 
 	})
 	.post(function(req, res){
-
+		var data = {
+			title: req.body.title
+		}
+		var image = new Image(data);
+		image.save(function(err){
+			if ( !err ) {
+				res.redirect("/app/imagenes/" + image._id);
+			}else{
+				res.render(err);
+			}
+		});
 	});
 
 module.exports = router;
